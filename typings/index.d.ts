9b6d735e3dff7f132dcf8bd6f5a4a08aa0011d3c
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-22 19:42:30 +0530

    typings: update typings
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,20 +2,15 @@
     directory?: string;
     defaultLocale?: string;
 }
-declare const _default: {
-    new (options?: LockeOptions): {
-        directory: string;
-        defaultLocale: string;
-        locales: string[];
-        constants: {
-            [key: string]: string;
-        };
-        strings: Map<string, Map<string, {
-            [key: string]: string;
-        }>>;
-        substitute(string: string, ...args: unknown[]): string;
-        getString(locale: string, namespace: string, key: string, ...args: unknown[]): string;
-    };
-};
-export = _default;
+declare class Locke {
+    private directory;
+    private defaultLocale;
+    private locales;
+    private constants;
+    private strings;
+    constructor(options?: LockeOptions);
+    private substitute;
+    getString(locale: string, namespace: string, key: string, ...args: unknown[]): string;
+}
+export { Locke };
 //# sourceMappingURL=index.d.ts.map
