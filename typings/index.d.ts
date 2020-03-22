e2bfbd63ea02132eee0c83221c90cd60b65c7599
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-03-22 19:17:40 +0530

    typings: add
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

---
+++ typings/index.d.ts
@@ -0,0 +1,21 @@
+﻿interface LockeOptions {
+    directory?: string;
+    defaultLocale?: string;
+}
+declare const _default: {
+    new (options?: LockeOptions): {
+        directory: string;
+        defaultLocale: string;
+        locales: string[];
+        constants: {
+            [key: string]: string;
+        };
+        strings: Map<string, Map<string, {
+            [key: string]: string;
+        }>>;
+        substitute(string: string, ...args: unknown[]): string;
+        getString(locale: string, namespace: string, key: string, ...args: unknown[]): string;
+    };
+};
+export = _default;
+//# sourceMappingURL=index.d.ts.map
